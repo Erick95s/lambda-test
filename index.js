240ddd234334e97a52dd57f15f94c94eace82f25
erick <thewarrior95eh@gmail.com>
2021-01-10 14:03:12 -0600

    moment

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+const moment = require("moment");
+
 exports.handler = async (event) =>{
     const response = {
         statusCode : 200,
-        body : JSON.stringify('Hola desde lambda!!')
+        body : JSON.stringify({fecha:moment().format('dddd')})
     };
     return response;
 }
